c7a7bc520728a04fe6517ffc4c7d278bbec73e2d
Liam <78104591+Liam-Nothing@users.noreply.github.com>
2023-01-31 14:30:59 +0100

    [Feature] Add base files

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="Content-Type" content="UTF-8">
+
+    <link rel="stylesheet" type="text/css" href="css/normalize.css">
+    <link rel="stylesheet" type="text/css" href="css/base.css">
+    <link rel="stylesheet" type="text/css" href="css/styles.css">
+    <link rel="stylesheet" type="text/css" href="css/print.css" media="print">
+
+    <title>Template presentation</title>
+</head>
+
+<body>
+    <header>
+
+    </header>
+    <main>
+
+    </main>
+    <footer>
+
+    </footer>
+    <script src="js/scripts.js"></script>
+</body>
+
+</html>
