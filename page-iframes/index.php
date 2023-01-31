80f41c3cb6f2c7c5ececcd09903ee7b6c16edeaf
Liam <78104591+Liam-Nothing@users.noreply.github.com>
2023-02-01 00:38:29 +0100

    [Feature] Add v1.0

---
+++ page-iframes/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="Content-Type" content="UTF-8">
+
+    <link rel="stylesheet" type="text/css" href="../css/normalize.css">
+    <link rel="stylesheet" type="text/css" href="../css/base.css">
+    <link rel="stylesheet" type="text/css" href="../css/styles.css">
+    <link rel="stylesheet" type="text/css" href="../css/print.css" media="print">
+
+    <title>Template presentation</title>
+</head>
+
+<body>
+    <header>
+
+    </header>
+    <main>
+        <iframe id="website_frame" src=""></iframe>
+        <section id="move">
+            <img src="https://nothingelse.fr/img/Logo_NothingElse_text.png">
+            <p>Templates</p>
+            <div>
+                <button id="backward" ><</button>
+                <button id="forward" >></button>
+            </div>
+            <p>
+                <span id="sites_length_nb" > 0 </span>
+                /
+                <span id="actual_site_nb"> 0 </span>
+            </p>
+        </section>
+    </main>
+    <footer>
+
+    </footer>
+    <script src="js/scripts.js"></script>
+</body>
+
+</html>
---
+++ page-iframes/js/scripts.js
@@ -0,0 +1,205 @@
+var website_list = [
+    "https://toidicode.github.io/template/01-foodee/",
+    "https://toidicode.github.io/template/02-tasty/",
+    // "https://toidicode.github.io/template/03-ethereal/",
+    // "https://toidicode.github.io/template/04-karmo/",
+    "https://toidicode.github.io/template/05-bodo/",
+    "https://toidicode.github.io/template/06-portfolio-master/",
+    "https://toidicode.github.io/template/07-snow-master/",
+    "https://toidicode.github.io/template/08-synthetica/",
+    // "https://toidicode.github.io/template/09-sprout-master/",
+    // "https://toidicode.github.io/template/10-bicycling-master/", NO
+    "https://toidicode.github.io/template/100-cookingschool/",
+    // "https://toidicode.github.io/template/11-megakit-master/",
+    "https://toidicode.github.io/template/12-garage/",
+    "https://toidicode.github.io/template/13-knight/",
+    "https://toidicode.github.io/template/14-new-age/",
+    "https://toidicode.github.io/template/15-treviso/",
+    "https://toidicode.github.io/template/16-story/",
+    "https://toidicode.github.io/template/17-cardio/",
+    "https://toidicode.github.io/template/18-infinity/",
+    "https://toidicode.github.io/template/19-made-one/",
+    // "https://toidicode.github.io/template/20-made-two/",
+    "https://toidicode.github.io/template/21-weather/",
+    "https://toidicode.github.io/template/22-john-doe/",
+    "https://toidicode.github.io/template/23-rage/",
+    "https://toidicode.github.io/template/24-solid-state/",
+    "https://toidicode.github.io/template/25-invention/",
+    "https://toidicode.github.io/template/26-exigo/",
+    "https://toidicode.github.io/template/27-logic/",
+    "https://toidicode.github.io/template/28-clemo/",
+    "https://toidicode.github.io/template/29-bino/",
+    "https://toidicode.github.io/template/30-hats/",
+    "https://toidicode.github.io/template/31-vira/",
+    "https://toidicode.github.io/template/32-landing-zero/",
+    // "https://toidicode.github.io/template/33-aircv/",
+    "https://toidicode.github.io/template/34-wow/",
+    "https://toidicode.github.io/template/35-volcan/",
+    "https://toidicode.github.io/template/36-rabbit/",
+    "https://toidicode.github.io/template/37-lazyfox/",
+    "https://toidicode.github.io/template/38-conference/",
+    "https://github.toidicode.com/39-sight/index-one-page.html",
+    "https://toidicode.github.io/template/40-metronic-shop-ui/theme/shop-index.html",
+    "https://toidicode.github.io/template/41-metronic-one-page/theme/index.html",
+    "https://toidicode.github.io/template/42-navigator-onepage/index.html",
+    "https://toidicode.github.io/template/43-metronic-one-page/theme/",
+    "https://toidicode.github.io/template/44-fame/",
+    "https://toidicode.github.io/template/45-themelight/",
+    "https://toidicode.github.io/template/46-plantilla/",
+    "https://toidicode.github.io/template/47-avana/",
+    // "https://toidicode.github.io/template/48-metronic-frontend/",
+    // "https://toidicode.github.io/template/49-asentus/",
+    "https://toidicode.github.io/template/50-airspace/",
+    // "https://toidicode.github.io/template/51-acidus/",
+    "https://toidicode.github.io/template/52-applayers/",
+    "https://toidicode.github.io/template/53-bizexpress/",
+    "https://toidicode.github.io/template/54-bizium/",
+    "https://toidicode.github.io/template/55-robot-factory/",
+    "https://toidicode.github.io/template/56-ghughu/",
+    "https://toidicode.github.io/template/57-texas-lawyer/",
+    "https://toidicode.github.io/template/58-lifetrackr/",
+    "https://toidicode.github.io/template/59-euro-travels/",
+    "https://toidicode.github.io/template/60-meatking/",
+    // "https://toidicode.github.io/template/61-mamma-s-kitchen/",
+    "https://toidicode.github.io/template/62-twenty/",
+    "https://toidicode.github.io/template/63-spectral/",
+    "https://toidicode.github.io/template/64-gentelella/",
+    "https://toidicode.github.io/template/65-boxer/",
+    // "https://toidicode.github.io/template/66-white-pro/",
+    "https://toidicode.github.io/template/67-awesome/",
+    "https://toidicode.github.io/template/68-johndoe/",
+    "https://toidicode.github.io/template/69-lucy/",
+    "https://toidicode.github.io/template/70-brandi/",
+    // "https://toidicode.github.io/template/71-meghna/",
+    "https://toidicode.github.io/template/72-navada-plus/",
+    "https://toidicode.github.io/template/73-rain/",
+    "https://toidicode.github.io/template/74-sports-coach/",
+    // "https://toidicode.github.io/template/75-agency/",
+    "https://toidicode.github.io/template/76-humanity/",
+    // "https://toidicode.github.io/template/77-imminent/",
+    "https://toidicode.github.io/template/78-evento/",
+    // "https://toidicode.github.io/template/79-layla/",
+    "https://toidicode.github.io/template/80-restaurant/",
+    "https://toidicode.github.io/template/81-travellers/",
+    "https://toidicode.github.io/template/82-restaurant/",
+    // "https://toidicode.github.io/template/83-fitness/",
+    "https://toidicode.github.io/template/84-creative/",
+    // "https://toidicode.github.io/template/85-awesome/",
+    "https://toidicode.github.io/template/86-photographer/",
+    "https://toidicode.github.io/template/87-luxury/",
+    "https://toidicode.github.io/template/88-darkjoe/",
+    // "https://toidicode.github.io/template/89-developer/",
+    // "https://toidicode.github.io/template/90-polo/",
+    "https://toidicode.github.io/template/91-renessa/",
+    "https://toidicode.github.io/template/92-office/",
+    "https://toidicode.github.io/template/93-flusk/",
+    // "https://toidicode.github.io/template/94-the-portfolio/",
+    "https://toidicode.github.io/template/95-initio/",
+    "https://toidicode.github.io/template/96-dolphin/",
+    "https://toidicode.github.io/template/97-soft-tech/",
+    "https://toidicode.github.io/template/98-mind-craft/",
+    // "https://toidicode.github.io/template/99-season/",
+    "https://toidicode.github.io/template/101-mobishop/",
+    "https://toidicode.github.io/template/102-awesome-portfolio/",
+    "https://toidicode.github.io/template/103-grocery-store/",
+    "https://toidicode.github.io/template/104-sass-landing-page/"
+]
+
+var actual_website = 0;
+
+if(getAllUrlParams(window.location.search).id <= website_list.length - 1) {
+    actual_website = getAllUrlParams(window.location.search).id;
+}
+
+document.getElementById("backward").addEventListener("click", backward);
+document.getElementById("forward").addEventListener("click", forward);
+
+render_website(actual_website);
+
+function backward() {
+    if (actual_website - 1 >= 0) {
+        actual_website--;
+        render_website(actual_website);
+    }
+}
+
+function forward() {
+    if (actual_website + 1 < website_list.length) {
+        actual_website++;
+        render_website(actual_website);
+    }
+}
+
+function render_website(id) {
+    document.getElementById("website_frame").src = website_list[id];
+    document.getElementById("sites_length_nb").innerHTML = id;
+    document.getElementById("actual_site_nb").innerHTML = website_list.length-1;
+    console.log(website_list[id]);
+}
+
+
+/* https://www.sitepoint.com/get-url-parameters-with-javascript/ */
+function getAllUrlParams(url) {
+
+    // get query string from url (optional) or window
+    var queryString = url ? url.split('?')[1] : window.location.search.slice(1);
+
+    // we'll store the parameters here
+    var obj = {};
+
+    // if query string exists
+    if (queryString) {
+
+        // stuff after # is not part of query string, so get rid of it
+        queryString = queryString.split('#')[0];
+
+        // split our query string into its component parts
+        var arr = queryString.split('&');
+
+        for (var i = 0; i < arr.length; i++) {
+            // separate the keys and the values
+            var a = arr[i].split('=');
+
+            // set parameter name and value (use 'true' if empty)
+            var paramName = a[0];
+            var paramValue = typeof (a[1]) === 'undefined' ? true : a[1];
+
+            // (optional) keep case consistent
+            paramName = paramName.toLowerCase();
+            if (typeof paramValue === 'string') paramValue = paramValue.toLowerCase();
+
+            // if the paramName ends with square brackets, e.g. colors[] or colors[2]
+            if (paramName.match(/\[(\d+)?\]$/)) {
+
+                // create key if it doesn't exist
+                var key = paramName.replace(/\[(\d+)?\]/, '');
+                if (!obj[key]) obj[key] = [];
+
+                // if it's an indexed array e.g. colors[2]
+                if (paramName.match(/\[\d+\]$/)) {
+                    // get the index value and add the entry at the appropriate position
+                    var index = /\[(\d+)\]/.exec(paramName)[1];
+                    obj[key][index] = paramValue;
+                } else {
+                    // otherwise add the value to the end of the array
+                    obj[key].push(paramValue);
+                }
+            } else {
+                // we're dealing with a string
+                if (!obj[paramName]) {
+                    // if it doesn't exist, create property
+                    obj[paramName] = paramValue;
+                } else if (obj[paramName] && typeof obj[paramName] === 'string') {
+                    // if property does exist and it's a string, convert it to an array
+                    obj[paramName] = [obj[paramName]];
+                    obj[paramName].push(paramValue);
+                } else {
+                    // otherwise add the property
+                    obj[paramName].push(paramValue);
+                }
+            }
+        }
+    }
+
+    return obj;
+}
